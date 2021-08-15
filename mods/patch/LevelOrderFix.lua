37aa8bec932984625b8ea2ba60dddd78b2e109b7
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-08-15 13:12:30 -0700

    Added "Level Order Fix" mod by VernonKun. This mod fixes the order of missions in the map viewer and lobby browser according to the lore, especially Reikwald Forest and River Reik which are listed in reverse order.

---
+++ mods/patch/LevelOrderFix.lua
@@ -0,0 +1,17 @@
+--[[
+  Name: Level Order Fix
+  Author: VernonKun
+  Updated: 5/30/2020
+
+  This mod fixes the order of missions in the map viewer and lobby browser according to the lore,
+  especially Reikwald Forest and River Reik which are listed in reverse order.
+--]]
+
+LevelSettings.dlc_dwarf_interior.map_settings.sorting = 19
+LevelSettings.dlc_dwarf_exterior.map_settings.sorting = 20
+LevelSettings.dlc_dwarf_beacons.map_settings.sorting = 21
+LevelSettings.dlc_stromdorf_hills.map_settings.sorting = 22
+LevelSettings.dlc_stromdorf_town.map_settings.sorting = 23
+LevelSettings.dlc_reikwald_forest.map_settings.sorting = 24
+LevelSettings.dlc_reikwald_river.map_settings.sorting = 25
+LevelSettings.dlc_challenge_wizard.map_settings.sorting = 4.5
