0cba2c4f3c37fc58fdd8444c204c3ce7281d1072
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-08-15 12:23:57 -0700

    Added "Sword And Shield Effect Fix" mod by VernonKun. Fixes the incorrect hit effect (decals and particles left to props when you hit them) on sword and shield. The original hit effect for swings #1 and #2 were that of the 2h hammers.

---
+++ mods/patch/SwordAndShieldEffectFix.lua
@@ -0,0 +1,20 @@
+--[[
+  Name: Sword And Shield Effect Fix
+  Author: VernonKun
+  Updated: 3/31/2020
+
+  Fixes the incorrect hit effect (decals and particles left to props when you hit them) on sword
+  and shield. The original hit effect for swings #1 and #2 were that of the 2h hammers.
+--]]
+
+Weapons.one_handed_sword_shield_template_1.actions.action_one.light_attack_left.hit_effect = "melee_hit_sword_1h"
+Weapons.one_handed_sword_shield_template_1.actions.action_one.light_attack_last.hit_effect = "melee_hit_sword_1h"
+
+Weapons.one_handed_sword_shield_template_1_co.actions.action_one.light_attack_left.hit_effect = "melee_hit_sword_1h"
+Weapons.one_handed_sword_shield_template_1_co.actions.action_one.light_attack_last.hit_effect = "melee_hit_sword_1h"
+
+Weapons.one_handed_sword_shield_template_1_t2.actions.action_one.light_attack_left.hit_effect = "melee_hit_sword_1h"
+Weapons.one_handed_sword_shield_template_1_t2.actions.action_one.light_attack_last.hit_effect = "melee_hit_sword_1h"
+
+Weapons.one_handed_sword_shield_template_1_t3.actions.action_one.light_attack_left.hit_effect = "melee_hit_sword_1h"
+Weapons.one_handed_sword_shield_template_1_t3.actions.action_one.light_attack_last.hit_effect = "melee_hit_sword_1h"
