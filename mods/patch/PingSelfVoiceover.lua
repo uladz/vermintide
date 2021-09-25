f1f3d51e2c08abe0722c8379dd31e3281c240826
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-09-25 15:39:10 -0700

    Added an option to disable voiceover and just ping yourself visually. Rewrote ping action function to properly send voiceover via RPC to a host. Fixed crash on host side due to assert rpc_play_dialogue_event.

--- mods/patch/PingSelfVoiceover.lua
+++ mods/patch/PingSelfVoiceover.lua
@@ -1,31 +1,54 @@
 --[[
-	Override the "ping self" voiceover:
-		This causes a predetermined line of dialogue to play when you ping yourself, instead
-		of a random line of "help me" dialogue.
+  Name: Ping Self Voiceover (ported from VMF)
+	Author: Walterr
+	Updated: uladz
+	Version: 1.2.1
+	Link: https://www.nexusmods.com/vermintide/mods/23
 
-	author: walterr
+	Override the "ping self" voiceover. This causes a predetermined line of dialogue to play when you
+	ping yourself, instead of a random line of "help me" dialogue. Options in Mod Settings -> Ping
+	Self Voiceover. Off by default. Home key (default) will ping self.
+
+	Version history:
+		1.0.0 Release.
+		1.1.0 Fix: This way can not be played in the inn.
+		1.2.0 Ported from VMF to QoL.
+		1.2.1 Added an option to disable voiceover and just ping yourself visually. Rewrote ping action
+			function to properly send voiceover via RPC to a host. Fixed crash on host side due to assert
+			rpc_play_dialogue_event.
 --]]
 
 local mod_name = "PingSelfVoiceover"
 PingSelfVoiceover = {}
 local mod = PingSelfVoiceover
 local oi = OptionsInjector
- 
+
 mod.widget_settings = {
-	DISABLED = {
-		["save"] = "cb_ping_self_voiceover_off",
+	ACTIVE = {
+		["save"] = "cb_ping_self_active",
+		["widget_type"] = "stepper",
+		["text"] = "Enable Ping Self Feature",
+		["tooltip"] =  "Enable Ping Self Feature\n" ..
+			"Toggle pinging self feature on / off.",
+		["value_type"] = "boolean",
+		["options"] = {
+			{text = "Off", value = false},
+			{text = "On", value = true},
+		},
+		["default"] = 2, -- Default second option is enabled. In this case On
+	},
+	VOICEOVER = {
+		["save"] = "cb_ping_self_voiceover",
 		["widget_type"] = "stepper",
 		["text"] = "Override Ping Self Voiceover",
 		["tooltip"] =  "Override Ping Self Voiceover\n" ..
-			"Toggle override of voiceover when pinging self on / off.\n\n" ..
-			"Changes your voiceover when you ping yourself to a \"This way\" message (only works" ..
-			"if the host has this mod enabled).",
+			"Enables voiceover when you ping yourself, will play \"This way\" message.",
 		["value_type"] = "boolean",
 		["options"] = {
-			{text = Localize("vmf_text_core_off"), value = false},
-			{text = Localize("vmf_text_core_on"), value = true},
+			{text = "Off", value = false},
+			{text = "On", value = true},
 		},
-		["default"] = 1, -- Default second option is enabled. In this case On
+		["default"] = 2, -- Default second option is enabled. In this case On
 	},
 	HK_TOGGLE = {
 		["save"] = "cb_ping_self_hotkey_ping",
@@ -35,29 +58,70 @@ mod.widget_settings = {
 			"home",
 			oi.key_modifiers.NONE,
 		},
-		["exec"] = {"PingSelfVoiceover", "action/ping_self"},
+		["exec"] = {"patch/action", "ping_self"},
 	},
 }
 
 mod.voice_settings = {
-	witch_hunter = { name = "pwh_objective_correct_path_this_way", index = 1 },
-	empire_soldier = { name = "pes_objective_correct_path_this_way", index = 1 },
-	dwarf_ranger = { name = "pdr_objective_correct_path_this_way", index = 3 },
-	bright_wizard = { name = "pbw_objective_correct_path_this_way", index = 1 },
-	wood_elf = { name = "pwe_objective_correct_path_this_way", index = 1 }
+	witch_hunter = {
+		name = "pwh_objective_correct_path_this_way",
+		index = 1
+	},
+	empire_soldier = {
+		name = "pes_objective_correct_path_this_way",
+		index = 1
+	},
+	dwarf_ranger = {
+		name = "pdr_objective_correct_path_this_way",
+		index = 3
+	},
+	bright_wizard = {
+		name = "pbw_objective_correct_path_this_way",
+		index = 1
+	},
+	wood_elf = {
+		name = "pwe_objective_correct_path_this_way",
+		index = 1
+	},
 }
 
--- ####################################################################################################################
--- ##### Options ######################################################################################################
--- ####################################################################################################################
-mod.create_options = function()
-	Mods.option_menu:add_group("ping_self", "Ping Self Voiceover")
+--[[
+  Options
+]]--
 
-	--Mods.option_menu:add_item("ping_self", mod.widget_settings.DISABLED, true)
-	Mods.option_menu:add_item("ping_self", mod.widget_settings.HK_TOGGLE, true)
+mod.create_options = function()
+	local group = "ping_self"
+	Mods.option_menu:add_group(group, "Ping Self Voiceover")
+	Mods.option_menu:add_item(group, mod.widget_settings.ACTIVE, true)
+	Mods.option_menu:add_item(group, mod.widget_settings.VOICEOVER)
+	Mods.option_menu:add_item(group, mod.widget_settings.HK_TOGGLE)
 end
 
--- ####################################################################################################################
--- ##### Start ########################################################################################################
--- ####################################################################################################################
-mod.create_options()
+--[[
+  Hooks
+]]--
+
+Mods.hook.set(mod_name, "DialogueSystem.rpc_play_dialogue_event", function(orig_func, self, sender, go_id, is_level_unit, dialogue_id, dialogue_index)
+	if self.is_server then
+		local network = Managers.state.network
+		local dialogue_name = NetworkLookup.dialogues[dialogue_id]
+		local dialogue = self.dialogues[dialogue_name]
+		local sound_event = dialogue.sound_events[dialogue_index]
+		local dialogue_actor_unit = network:game_object_or_level_unit(go_id, is_level_unit)
+		local extension = self.unit_extension_data[dialogue_actor_unit]
+		local wwise_source_id = WwiseUtils.make_unit_auto_source(self.world, extension.play_unit, extension.voice_node)
+		local source_id = self.wwise_world:trigger_event(sound_event, wwise_source_id)
+		network.network_transmit:send_rpc_clients("rpc_play_dialogue_event", go_id, is_level_unit, dialogue_id, dialogue_index)
+	else
+		return orig_func(self, sender, go_id, is_level_unit, dialogue_id, dialogue_index)
+	end
+end)
+
+--[[
+  Start
+]]--
+
+local status, err = pcall(mod.create_options)
+if err ~= nil then
+	EchoConsole(err)
+end
--- mods/patch/action/ping_self.lua
+++ mods/patch/action/ping_self.lua
@@ -1,32 +1,34 @@
-local mod, mod_name, oi = Mods.get_mod("PingSelfVoiceover")
-
 safe_pcall(function()
+	if not Application.user_setting(PingSelfVoiceover.widget_settings.ACTIVE.save) then
+		return
+	end
+
 	local local_player = Managers.player:local_player()
-	
 	if local_player then
+
 		local local_player_unit = local_player.player_unit
-		
 		if local_player_unit and Unit.alive(local_player_unit) then
-			local unit_id = Managers.state.network:unit_game_object_id(local_player_unit)
+
 			local local_character = SPProfiles[local_player.profile_index].character_vo
-			local voice_setting = mod.voice_settings[local_character]
-			
+			local voice_setting = PingSelfVoiceover.voice_settings[local_character]
 			local dialogue_system = Managers.state.entity._systems.dialogue_system
-			
-			if dialogue_system.dialogues[voice_setting.name] then
-				local dialogue = NetworkLookup.dialogues[voice_setting.name]
-				local go_id, is_level_unit = Managers.state.network:game_object_or_level_id(unit_id)
-				
-				local human_players = Managers.player:human_players()
-				for _, player in pairs(human_players) do
-					-- Ping yourself
-					Managers.state.network.network_transmit:send_rpc(
-						"rpc_ping_unit", player.peer_id, unit_id, unit_id)
-					
+			local dialogue = dialogue_system.dialogues[voice_setting.name]
+
+			if dialogue then
+				local network = Managers.state.network
+				local unit_id = network:unit_game_object_id(local_player_unit)
+
+				-- Ping yourself
+				network.network_transmit:send_rpc_server("rpc_ping_unit", unit_id, unit_id)
+
+				if Application.user_setting(PingSelfVoiceover.widget_settings.VOICEOVER.save) then
+					local dialogue_id = NetworkLookup.dialogues[voice_setting.name]
+					local dialogue_index = voice_setting.index
+					local go_id, is_level_unit = network:game_object_or_level_id(unit_id)
+
 					-- Play dialogue
-					Managers.state.network.network_transmit:send_rpc(
-						"rpc_play_dialogue_event", player.peer_id,
-						go_id, is_level_unit, dialogue, voice_setting.index)
+					network.network_transmit:send_rpc_server("rpc_play_dialogue_event",
+						go_id, is_level_unit, dialogue_id, dialogue_index)
 				end
 			end
 		end
