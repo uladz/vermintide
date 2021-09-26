ef5c98e94eaf0035aac2a2844ab0f57d89ec794f
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-09-25 21:31:58 -0700

    Refactored code and changed options to better fit QoL mod options.

--- mods/patch/PotionDialogue.lua
+++ mods/patch/PotionDialogue.lua
@@ -1,45 +1,36 @@
 --[[
-	author: Aussiemon
-	
-	-----
- 
-	Copyright 2018 Aussiemon
+	Name: Potion Dialogue
+	Author: Aussiemon
+	Updated: uladz (since 1.1.0)
+	Version: 1.1.0 (9/25/2021)
+	Link: https://github.com/Aussiemon/Vermintide-JHF-Mods/blob/original_release/mods/patch/PotionDialogue.lua
 
-	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
-	-----
-	
 	Plays unused character-specific dialogue when a potion is consumed.
+
+	Version history:
+		1.0.0 Uptaken from GIT, last update 2/6/2018.
+		1.1.0 Refactored code and changed options to better fit QoL mod options.
 --]]
 
 local mod_name = "PotionDialogue"
-
--- ##########################################################
--- ################## Variables #############################
-
-PotionDialogue = {
-	SETTINGS = {
-		ACTIVE = {
-			["save"] = "cb_potion_dialogue_enabled",
-			["widget_type"] = "stepper",
-			["text"] = "Potion Dialogue - Enabled",
-			["tooltip"] = "Potion Dialogue\n" ..
-				"Plays unused character-specific dialogue when a potion is consumed.",
-			["value_type"] = "boolean",
-			["options"] = {
-				{text = "Off", value = false},
-				{text = "On", value = true}
-			},
-			["default"] = 1, -- Default second option is enabled. In this case Off
-		}
-	}
-}
-
+PotionDialogue = {}
 local mod = PotionDialogue
+
+mod.widget_settings = {
+	ACTIVE = {
+		["save"] = "cb_potion_dialogue_enabled",
+		["widget_type"] = "stepper",
+		["text"] = "Potion Dialogue",
+		["tooltip"] = "Potion Dialogue\n" ..
+			"Plays unused character-specific dialogue when a potion is consumed.",
+		["value_type"] = "boolean",
+		["options"] = {
+			{text = "Off", value = false},
+			{text = "On", value = true}
+		},
+		["default"] = 1, -- Default second option is enabled. In this case Off
+	},
+}
 
 mod.stance_equivalents_all = {
 	damage_boost_potion_weak = "offensive",
@@ -61,15 +52,21 @@ local ScriptUnit_extension_input = ScriptUnit.extension_input
 local FrameTable_alloc_table = FrameTable.alloc_table
 local Application_user_setting = Application.user_setting
 
--- ##########################################################
--- ################## Functions #############################
+--[[
+  Options
+]]--
 
-mod.create_options = function ()
-	Mods.option_menu:add_group("potion_dialogue", "Potion Dialogue")
-	Mods.option_menu:add_item("potion_dialogue", mod.SETTINGS.ACTIVE, true)
+mod.create_options = function()
+	local group = "tweaks"
+	Mods.option_menu:add_group(group, "Gameplay Tweaks")
+	Mods.option_menu:add_item(group, mod.widget_settings.ACTIVE, true)
 end
 
-mod.trigger_potion_dialogue_event = function (action_extension)
+--[[
+  Functions
+]]--
+
+mod.trigger_potion_dialogue_event = function(action_extension)
 	local current_action = action_extension.current_action
 	local owner_unit = action_extension.owner_unit
 	local buff_template = current_action.buff_template
@@ -83,29 +80,27 @@ mod.trigger_potion_dialogue_event = function (action_extension)
 	end
 end
 
-local get = function (data)
+local get = function(data)
 	return Application_user_setting(data.save)
 end
-local set = Application.set_user_setting
-local save = Application.save_user_settings
 
--- ##########################################################
--- #################### Hooks ###############################
+--[[
+  Hooks
+]]--
 
 Mods.hook.set(mod_name, "ActionPotion.finish", function (func, self, reason, ...)
-	
+
 	-- Play dialogue events
-	if get(mod.SETTINGS.ACTIVE) and reason == "action_complete" then
+	if get(mod.widget_settings.ACTIVE) and reason == "action_complete" then
 		mod.trigger_potion_dialogue_event(self)
 	end
-	
+
 	local result = func(self, reason, ...)
 	return result
 end)
 
--- ##########################################################
--- ################### Script ###############################
+--[[
+  Start
+]]--
 
 mod.create_options()
-
--- ##########################################################
