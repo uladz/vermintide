78ab69276f11970c0ea03782975d140447951279
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-09-28 19:00:46 -0700

    Code cleanup, menu option moved to "Gameplay Tweaks" to better fit QoL.

--- mods/patch/AllReady.lua
+++ mods/patch/AllReady.lua
@@ -1,11 +1,27 @@
 --[[
-	Patch all_peers_ready:
-		This allows you to start the game without other people have to vote.
+	Name: All Ready
+	Author: IamLupo
+	Updated: uladz (since 1.1.0)
+	Version: 1.1.1 (9/25/2021)
+	Link: https://www.nexusmods.com/vermintide/mods/4
+
+	This mod allows you to start the game without having to wait for other people to Ready up (F2).
+	Toggle this mod on and off in Mod Settings -> Gameplay Tweaks -> All Ready. Off by default.
+
+	Version history:
+		1.0.0 Release.
+		1.0.1 Added german language.
+		1.1.0 Ported to QoL from VMF, only english.
+		1.1.1 Code cleanup, menu option moved to "Gameplay Tweaks" to better fit QoL.
 --]]
 
 local mod_name = "AllReady"
 AllReady = {}
 local mod = AllReady
+
+--[[
+  Variables
+]]--
 
 mod.widget_settings = {
 	ACTIVE = {
@@ -23,29 +39,40 @@ mod.widget_settings = {
 	},
 }
 
-mod.get = Application.user_setting
+--[[
+  Options
+]]--
 
--- ####################################################################################################################
--- ##### Options ######################################################################################################
--- ####################################################################################################################
 mod.create_options = function()
-	Mods.option_menu:add_group("system", "System")
-
-	Mods.option_menu:add_item("system", mod.widget_settings.ACTIVE, true)
+	local group = "tweaks"
+	Mods.option_menu:add_group(group, "Gameplay Tweaks")
+	Mods.option_menu:add_item(group, mod.widget_settings.ACTIVE, true)
 end
 
--- ####################################################################################################################
--- ##### Hook #########################################################################################################
--- ####################################################################################################################
+--[[
+  Functions
+]]--
+
+mod.get = function(data)
+	if data then
+		return Application.user_setting(data.save)
+	end
+end
+
+--[[
+  Hooks
+]]--
+
 Mods.hook.set(mod_name, "MatchmakingManager.all_peers_ready", function(func, ...)
-	if mod.get(mod.widget_settings.ACTIVE.save) then
+	if mod.get(mod.widget_settings.ACTIVE) then
 		return true
 	else
 		return func(...)
 	end
 end)
 
--- ####################################################################################################################
--- ##### Start ########################################################################################################
--- ####################################################################################################################
+--[[
+  Start
+]]--
+
 mod.create_options()
