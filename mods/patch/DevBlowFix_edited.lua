5a356c41ce049c0be055294c036878384c9a828c
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-09-28 19:48:37 -0700

    Added comments about VernonKun's edits in DevBlowFix_edited.

--- mods/patch/DevBlowFix_edited.lua
+++ mods/patch/DevBlowFix_edited.lua
@@ -1,23 +1,19 @@
 ﻿--[[
-	_______________________________
-	DEVASTATING BLOW - OFF-HOST FIX
-	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-	
-	 v. 1.2.1
+	Name: Devastating Blow Fix (ported from VMF)
+	Author: Zeropathic
+	Version: 1.2.1
 
-	Written by Zeropathic
-	
-	
-	
-	What this mod does:
-	¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
-	
-	Devastating Blow doesn't only increase your push strength; it also increases the stagger strength of your weapon's attacks.
-	
-	Normally, this secondary effect only works if you're the host. This mod aims to fix this bug for clients connected to you.
-	
-	NOTE: Only the host needs to have this mod installed. It does (or at least should do) nothing if installed on clients.
-	
+	Devastating Blow doesn't only increase your push strength; it also increases the stagger strength
+	of your weapon's attacks. Normally, this secondary effect only works if you're the host. This mod
+	aims to fix this bug for clients connected to you.
+
+	NOTE: Only the host needs to have this mod installed. It does (or at least should do) nothing if
+	installed on clients.
+
+	Update 5/24/2020: Ported from VMF (by VernonKun).
+	Devastating blow has the effect of nullifying "ignore_stagger" of some enemy actions, e.g.
+	stormvermin overheads, but it only works for the host. This fix runs on the host side and gives
+	every client in the lobby the same effect if they have devastating blow.
 --]]
 
 
@@ -31,10 +27,10 @@ local function hasDevBlow(attacker_unit)
 	local inventory_extension = ScriptUnit.has_extension(attacker_unit, "inventory_system")
 	local wielded_slot = inventory_extension and inventory_extension:get_wielded_slot_name()
 	local slot_data = inventory_extension and inventory_extension:get_slot_data(wielded_slot)
-		
+
 	local item_key = slot_data and slot_data.item_data.key
 	local item = item_key and ItemMasterList[item_key]
-	
+
 	if item and item.traits then -- Ogres can trigger this function. This check keeps them out. It would crash because they don't have item data.
 		for _, trait_name in ipairs(item.traits) do
 			if trait_name == "melee_weapon_push_increase" then
@@ -69,19 +65,19 @@ Mods.hook.set(mod_name, "DamageUtils.calculate_stagger", function(func, damage_t
 	local blackboard = Unit.get_data(target_unit, "blackboard")
 	local action = blackboard.action
 	local ignore_staggers = action and action.ignore_staggers
-	
+
 	-- ignore_staggers seems to be active during stormvermin attacks
 	if ignore_staggers then
-		--[[	
+		--[[
 		-- Default stuff. Doesn't work when off-host is attacker
 		local owner_buff_extension = ScriptUnit.has_extension(attacker_unit, "buff_system")
-		
+
 		-- This check always returns false if a client is the attacker. It's as if buff data isn't properly communicated to the host or something.
 		if owner_buff_extension and owner_buff_extension:has_buff_type("push_increase") then
 			ignore_staggers = false
 		end
 		--]]
-		
+
 		-- My own check. See the function above.
 		if hasDevBlow(attacker_unit) then
 			ignore_staggers = false
@@ -118,7 +114,7 @@ Mods.hook.set(mod_name, "DamageUtils.calculate_stagger", function(func, damage_t
 			end
 		end
 	end
-	
+
 	return stagger, duration
 end)
 --
