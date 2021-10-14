2995ddfbb40deb331673318809b9074a5fb9663b
Vlad Sharanhovich <vlad.sharanhovich@gmail.com>
2021-10-13 19:42:49 -0700

    Added configuration for position and size of the indicator.

--- mods/patch/ChargeLevelIndicator.lua
+++ mods/patch/ChargeLevelIndicator.lua
@@ -1,27 +1,38 @@
 --[[
-	Name: Charge Level Indicator (ported from VMF)
+	Name: Charge Level Indicator
 	Authors: walterr
-	Updated by: uladz (since 1.2.1)
-	Version: 1.2.1 (9/22/2021)
+	Updated by: uladz (since 1.3.0)
+	Version: 1.3.2 (9/22/2021)
 
+	Description:
 	When charging the Bolt staff or Fireball staff, a colored marker will be shown on the HUD
 	indicating the current 'charge level'. The Bolt staff has discrete charge levels, which are shown
 	as: green=1, orange=2, red=3. The Fireball staff doesn't have discrete levels and charging just
-	continuously increases damage and area, so the levels I've chosen are kind of arbitrary: green=(
-	3 <= damage < 5, 0.75 <= area < 2.25), orange=(5 <= damage < 6, 2.25 <= area < 3), red=(
-	damage = 6, area = 3). (Where 'damage' means AoE instantaneous damage to normal targets, not
-	including impact damage or DoT.)
+	continuously increases damage and area, so the levels I've chosen are kind of arbitrary: green=(3
+	<= damage < 5, 0.75 <= area < 2.25), orange=(5 <= damage < 6, 2.25 <= area < 3), red=( damage = 6,
+	area = 3). (Where 'damage' means AoE instantaneous damage to normal targets, not including impact
+	damage or DoT.)
+
+	This mod is intended to work with QoL modpack. To "install" copy the file to
+	"<game>\binaries\mods\patch" folder. To enable go to "Options" -> "Mod Settings" ->
+	"Trueflight Tweaks" and turn on "Show Charge Level Indicator" option. Off by default.
 
 	Version history:
-	1.2.1 Added option to enable/disable this tweak in "Trueflight Tweaks".
-	1.2.0 Update walters script to new structure.
-	1.1.0 Walter uploaded his script to moddb.
 	1.0.0 Release.
+	1.1.0 Walter uploaded his script to moddb.
+	1.2.0 Update walters script to new structure.
+	1.3.0 Ported from VMF 0.17.4 to QoL.
+	1.3.1 Added option to enable/disable this tweak in "Trueflight Tweaks".
+	1.3.2 Added configuration for position and size of the indicator.
 --]]
 
 local mod_name = "ChargeLevelIndicator"
 ChargeLevelIndicator = {}
 local mod = ChargeLevelIndicator
+
+--[[
+  Variables
+--]]
 
 mod.widget_settings = {
 	ACTIVE = {
@@ -37,6 +48,50 @@ mod.widget_settings = {
 			{text = "On", value = true},
 		},
 		["default"] = 1, -- Default first option is enabled. In this case Off
+		["hide_options"] = {
+			{
+				false,
+				mode = "hide",
+				options = {
+					"cb_charge_level_indicator_position",
+					"cb_charge_level_indicator_size",
+				},
+			}, {
+				true,
+				mode = "show",
+				options = {
+					"cb_charge_level_indicator_position",
+					"cb_charge_level_indicator_size",
+				},
+			},
+		},
+	},
+	POSITION = {
+		["save"] = "cb_charge_level_indicator_position",
+		["widget_type"] = "stepper",
+		["text"] = "Indicator Position",
+		["tooltip"] = "Indicator Position\n" ..
+			"",
+		["value_type"] = "number",
+		["options"] = {
+			{text = "Below", value = 1},
+			{text = "Above", value = 2},
+		},
+		["default"] = 1, -- Default first option is enabled. In this case "Below"
+	},
+	SIZE = {
+		["save"] = "cb_charge_level_indicator_size",
+		["widget_type"] = "stepper",
+		["text"] = "Indicator Size",
+		["tooltip"] = "Indicator Size\n" ..
+			"",
+		["value_type"] = "number",
+		["options"] = {
+			{text = "Large", value = 1},
+			{text = "Medium", value = 2},
+			{text = "Small", value = 3},
+		},
+		["default"] = 1, -- Default first option is enabled. In this case "Large"
 	},
 	-- The charge level indicator is just a rounded rect. The color is set dynamically.
 	CHARGE_LEVEL = {
@@ -52,7 +107,7 @@ mod.widget_settings = {
 		content = {},
 		style = {
 			indicator = {
-				offset = { 230, -45 },
+				offset = { 232, -45 }, -- below
 				size = { 48, 48 },
 				corner_radius = 3,
 			},
@@ -110,6 +165,9 @@ mod.display_info = {
 	},
 }
 
+-- widget refresh rate
+mod.widget_config_refresh = 0
+
 --[[
   Options
 --]]
@@ -118,6 +176,8 @@ mod.create_options = function()
 	local group = "trueflight_tweaks"
 	Mods.option_menu:add_group(group, "Trueflight Tweaks")
 	Mods.option_menu:add_item(group, mod.widget_settings.ACTIVE, true)
+	Mods.option_menu:add_item(group, mod.widget_settings.POSITION)
+	Mods.option_menu:add_item(group, mod.widget_settings.SIZE)
 end
 
 --[[
@@ -125,7 +185,9 @@ end
 --]]
 
 mod.get = function(data)
-	return Application.user_setting(data.save)
+	if data then
+		return Application.user_setting(data.save)
+	end
 end
 
 mod.display_info.ActionCharge.compute_level_value = function(action)
@@ -265,6 +327,39 @@ Mods.hook.set(mod_name, "OverchargeBarUI.update", function(func, self, dt, ...)
 				widget.style.indicator.color = mod.current_charge_level.color
 			end
 
+			mod.widget_config_refresh = mod.widget_config_refresh - 1
+			if mod.widget_config_refresh < 0 then
+				mod.widget_config_refresh = 100
+
+				local pos = mod.get(mod.widget_settings.POSITION)
+				local size = mod.get(mod.widget_settings.SIZE)
+
+				-- change indicator position and size as configured
+				if pos == 1 then
+					if size == 1 then
+						widget.style.indicator.offset = {232, -45}
+						widget.style.indicator.size = {48, 48}
+					elseif size == 2 then
+						widget.style.indicator.offset = {238, -32}
+						widget.style.indicator.size = {36, 36}
+					else
+						widget.style.indicator.offset = {244, -20}
+						widget.style.indicator.size = {24, 24}
+					end
+				else
+					if size == 1 then
+						widget.style.indicator.offset = {232, 32}
+						widget.style.indicator.size = {48, 48}
+					elseif size == 2 then
+						widget.style.indicator.offset = {238, 30}
+						widget.style.indicator.size = {36, 36}
+					else
+						widget.style.indicator.offset = {244, 29}
+						widget.style.indicator.size = {24, 24}
+					end
+				end
+			end
+
 			-- Draw the charge level indicator.
 			local input_service = self.input_manager:get_service("ingame_menu")
 			local ui_renderer = self.ui_renderer
@@ -279,4 +374,7 @@ end)
 	Start
 --]]
 
-mod.create_options()
+local status, err = pcall(mod.create_options)
+if err ~= nil then
+	EchoConsole(err)
+end
